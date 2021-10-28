e82c99d135e23e5465ffb0b80ff23efafdf66b03
Chun Hong <junhong.low123@gmail.com>
2021-10-28 22:53:21 +0800

    Changing

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -25,12 +25,8 @@ class HomeController @Inject()(cc: ControllerComponents) (implicit assetsFinder:
    * a path of `/`.
    */
   private val samples = mutable.ArrayBuffer(
-    Sample("Low Chun Hong", 1001, 1000, "Puchong"),
-    Sample("Soh Kah Lok", 1002, 1500, "Klang"),
-    Sample("Shantini", 1003, 1200, "Kepong")
+    Sample("Low Chun Hong", 1001, 1000, "Puchong")
   )
-
-  private val postUpdate = routes.HomeController.editSample
 
   def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
@@ -41,20 +37,20 @@ class HomeController @Inject()(cc: ControllerComponents) (implicit assetsFinder:
   }
 
   def createSample(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.createSample())
+    Ok(views.html.createSample(form))
   }
 
   def editSample(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.editSample())
+    Ok(views.html.editSample(form))
   }
 
   val form = Form(
     mapping(
       "fullName" -> nonEmptyText,
-      "empCode" -> number(min = 0),
+      "empCode" -> number(min = 4),
       "salary" -> number(min = 0),
       "city" -> nonEmptyText
-    )(Data.apply)(Data.unapply)
+    )(Sample.apply)(Sample.unapply)
   )
 
   def save = Action { implicit request: Request[AnyContent] =>
@@ -64,14 +60,38 @@ class HomeController @Inject()(cc: ControllerComponents) (implicit assetsFinder:
         Redirect(routes.HomeController.createSample)
       },
       success = { data =>
-        samples += Sample(data.fullName, data.empCode, data.salary, data.city)
-        Redirect(routes.HomeController.listSample)
+        samples.find(_.empCode == data.empCode) match {
+          case Some(t) =>
+            Redirect(routes.HomeController.createSample).flashing("ERROR" -> "Duplicate Emp code")
+          case None =>
+            samples.find(_.fullName == data.fullName) match{
+              case Some (a) =>
+                Redirect(routes.HomeController.createSample).flashing("ERROR" -> "Duplicate Name")
+              case None =>
+                samples += Sample(data.fullName, data.empCode, data.salary, data.city)
+                Redirect(routes.HomeController.listSample).flashing("SUCCESS" -> "Data created successfully")
+            }
+        }
       }
     )
   }
 
   def edit(code: Int): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    println(code)
-    Redirect(routes.HomeController.editSample)
+    val editData = samples.find(_.empCode == code)
+    println(editData)
+    editData match {
+      case Some(a) =>
+        println(a)
+        Redirect(routes.HomeController.listSample).flashing("ERROR" -> "Not Match")
+      case None =>
+        Redirect(routes.HomeController.editSample).flashing("SUCCESS" -> "Match")
+    }
+  }
+
+  def delete(code: Int): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    val del = samples.find(_.empCode == code)
+    println(del)
+    del.map(d => samples.remove(samples.indexOf(d)))
+    Redirect(routes.HomeController.listSample)
   }
 }
